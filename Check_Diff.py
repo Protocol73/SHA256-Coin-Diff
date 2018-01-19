3012f8bd0f1582f3cbf27de48ad9ffeb6ca97505
Protocol73 <mlaw4321@gmail.com>
2018-01-19 05:56:01 -0600

    Added settings
    and formatted comments

--- Check_Diff.py
+++ Check_Diff.py
@@ -3,17 +3,22 @@ import urllib.parse
 import requests
 import time
 
+#Settings 
+#open time-\
+delay1 = 10
+#open time-/
+
+#url settings-\
 chainz_api = 'https://chainz.cryptoid.info/xjo/api.dws?'
-
 check = 'summary'
-
 url = chainz_api + urllib.parse.urlencode({'q': check})
-
 json_data = requests.get(url).json()
-
+#url settings-/
+#coin settings-\
 zet_coin_raw = json_data['zet']['diff']
 xjo_coin_raw = json_data['xjo']['diff']
 v_coin_raw = json_data['vcn']['diff']
+#coin settings-/
 
 #remove dec
 v_coin = int(v_coin_raw)
@@ -25,4 +30,4 @@ print("Vcoin Difficulty: " + str(v_coin))
 print("JouleCoin Difficulty: " + str(xjo_coin)) 
 print("Zetacoin Difficulty: " + str(zet_coin))
 
-time.sleep(10)
+time.sleep(delay1)
